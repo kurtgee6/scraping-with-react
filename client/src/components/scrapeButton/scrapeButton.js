1514d7f98a1794f5a60bf0cdcc90777437dc2c28
kurtgee6 <kurtgee11@gmail.com>
2017-10-08 22:44:18 -0400

    major update

--- client/src/components/scrapeButton/scrapeButton.js
+++ client/src/components/scrapeButton/scrapeButton.js
@@ -13,7 +13,6 @@ class scrapeButton extends Component {
    render () {
         return (
             <div>
-                <input type="text"></input>
                 <div id="buttContainer">
                 <button id="scrapeArt">Scrape Sports Articles</button> 
                 </div>
